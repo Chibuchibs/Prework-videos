488c8dabf255f23018e878b02729f07ffcaf5505
Damian Amajoyi <damianamajoyi@Damians-MacBook-Pro.local>
2026-04-12 16:34:45 -0700

    initial commit

---
+++ Introduction.py
@@ -0,0 +1,31 @@
+# variables for user intro
+first_name = "Marie"
+last_name = "Garcia"
+age = 28
+City = "Denver"
+favorite_language = "Python"
+
+
+
+print("="*40)
+
+print("Personal introduction")
+
+print("="*40)
+
+# output for user info
+print("\nname:", first_name, last_name)
+
+print("city:", City)
+
+print("favorite language:", favorite_language)
+
+print("="*40)
+
+# maria garcia is 28 years old and lives in denver.
+
+print(f"{first_name} {last_name} is {age} years old and lives in {City}")
+
+
+
+
---
+++ about_me.py
@@ -0,0 +1,24 @@
+# This program introduces myself and shares some information about me.
+
+first_name = "chibu"
+last_name = "Amajoyi"
+age = 43
+City = "san francisco"
+current_job = "healthcare manager"
+
+print("="*40)
+print("Personal introduction")
+print("="*40)
+
+print("Name:", first_name, last_name)
+print("Age:", age)
+print("City:", City)
+print("Current job:", current_job)
+print("="*40)
+
+# why I'm learning to code
+print("I'm learning to code because I want to transition into a career in software development and build AI products.")
+
+# A fun fact about me
+print("A fun fact about me is that I have been passionate about technology and software development for many years.")
+print("="*40)
